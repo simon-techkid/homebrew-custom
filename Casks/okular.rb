dc7fc0fee6b98b0ec2bb4dba112a3a5a6fe18c3a
simon-techkid <simon.techkid@outlook.com>
2025-10-17 17:51:43 -0700

    Added Okular Cask

---
+++ Casks/okular.rb
@@ -0,0 +1,23 @@
+cask "okular" do
+  version :latest
+  sha256 :no_check
+
+  url "https://cdn.kde.org/ci-builds/graphics/okular/master/macos-arm64/okular-master-6812-macos-clang-arm64.dmg"
+  name "Okular (CI build master)"
+  desc "KDE document viewer - CI/bleeding-edge macOS ARM build"
+  homepage "https://kde.org/applications/graphics/okular/"
+
+  app "Okular.app"
+
+  # Optionally allow auto-updates / version checks:
+  livecheck do
+    url "https://cdn.kde.org/ci-builds/graphics/okular/master/macos-arm64/"
+    regex(/okular-master-(\d+)-macos-clang-arm64\.dmg/i)
+  end
+
+  # Cleanup (if there were supporting files or symlinks):
+  zap trash: [
+    "~/Library/Application Support/Okular",
+    "~/Library/Preferences/org.kde.okular.plist",
+  ]
+end
